d5d17351073ebdc8e7862e8a6567fd40c0e35aae
Nectarios Chroniaris <nchroniaris@gmail.com>
2021-09-11 11:07:34 -0600

    add serializers, add framework

---
+++ backend/PlotYourVibe/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import *
+
+
+class MoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mood
+        fields = (
+            'id',
+            'type',
+            'scale',
+            'description',
+            'timestamp'
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'name',
+            'level'
+        )
