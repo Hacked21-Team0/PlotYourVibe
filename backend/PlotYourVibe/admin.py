7cdcee8daa6f4cafd0b045caeb109510869fd8d6
Jingyu Xiang <jxiang2@ualberta.ca>
2021-09-11 10:48:55 -0600

    models and admin

--- backend/PlotYourVibe/admin.py
+++ backend/PlotYourVibe/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.db import models
+from . import models
+
 
 # Register your models here.
+
+#Mood
+@admin.register(models.Mood)
+class MoodAdmin(admin.ModelAdmin):
+    list_display = ['mood', 'scale', 'description', 'user']
+    list_filter = ['mood']
+
+    def mood_user(self, mood):
+        return mood.user.name
+
+@admin.register(models.User)
+class MoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'weight_kg', 'height_cm', 'level']
+    list_filter = ['name']
--- backend/PlotYourVibe/models.py
+++ backend/PlotYourVibe/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.db.models.fields import CharField, DecimalField, EmailField, FloatField
+#from django.contrib.auth.models import User
 
 # Create your models here.
+
+class User(models.Model):
+    LEVEL_1 = '1'
+    LEVEL_2 = '2'
+    LEVEL_3 = '3'
+
+    #Level1 is entry level users, Level3 is for high level users
+    LEVEL_CHOICES = [
+        (LEVEL_1, 'Level 1'),
+        (LEVEL_2, 'Level 2'),
+        (LEVEL_3, 'Level 3'),
+    ]
+
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    weight_kg = DecimalField(max_digits=6, 
+        decimal_places=2, default=100)
+    height_cm = DecimalField(max_digits=6, 
+        decimal_places=2, default=100)
+    level = models.CharField(max_length=1, choices=LEVEL_CHOICES, default=LEVEL_1)
+
+    def __str__(self) -> str:
+        return self.name
+    class Meta:
+        ordering = ['name']
+
+
+
+class Mood(models.Model):
+    TYPE_HAPPY = 'H'
+    TYPE_EXCITED = 'E'
+    TYPE_ANGRY = 'A'
+    TYPE_DEPRESSED = 'D'
+    TYPE_IRRITATED = 'I'
+    TYPE_SAD = 'S'
+
+    MOOD_CHOICES = [
+        (TYPE_HAPPY, 'Happy'),
+        (TYPE_EXCITED, 'Excited'),
+        (TYPE_ANGRY, 'Angry'),
+        (TYPE_DEPRESSED, 'Depressed'),
+        (TYPE_IRRITATED, 'Irritated'),
+        (TYPE_SAD, 'Sad')
+    ]
+
+    mood = models.CharField(max_length=1, choices=MOOD_CHOICES, default=TYPE_HAPPY)
+    scale = models.DecimalField(max_digits=2, decimal_places=2)
+    description = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # (User)1 -- *(Mood)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.mood
+    class Meta:
+        ordering = ['mood']
+
+
+    
+
+
