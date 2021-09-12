0c6183a4fa7bed79859c7b8e10b836c341440826
pickabo <nhossain@ualberta.ca>
2021-09-12 11:16:25 -0600

    put the react nav bar in

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import NavBar from '../components/NavBar'
 import Barchart from '../components/Barchart'
 import History from './History'
 import '../styles/dashboard.css'
@@ -6,11 +7,9 @@ import '../styles/dashboard.css'
 export default class Dashboard extends Component {
 	render() {
 		return (<div className='bg'>
-			<div class="navbar">
-				<a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
-				<a href="#"><i class="fa fa-fw fa-envelope"></i> Charts</a> 
-				<a href="#"><i class="fa fa-fw fa-user"></i> Sign Out</a>
-				</div>
+			
+				<NavBar/>
+
 			<div className='header'>
 				Mood Dashboard
 			</div>
