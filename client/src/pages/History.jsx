aa486ae6637e900fd2105a031619352ca1341420
adit95 <ferdousadit99@gmail.com>
2021-09-12 10:29:04 -0600

    Conditional statement accordign to entries in the table

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -7,18 +7,32 @@ export default class History extends Component {
   };
 
   handleDelete = (historyData) => {
-    const editedHistoryList = this.state.history.filter(m => m._id !== historyData._id);
-    this.setState({ history: editedHistoryList })
-
+    const editedHistoryList = this.state.history.filter(
+      (m) => m._id !== historyData._id
+    );
+    this.setState({ history: editedHistoryList });
   };
+
   render() {
+    const { length: count } = this.state.history;
+    if (count === 0)
+      return (
+        <h5>There are no mood entries. Try it. Tell us about your mood.</h5>
+      );
+
+    const thisPageCSS = {
+      marginLeft: 50,
+      marginRight: 50,
+      marginTop: 20,
+    };
+
     return (
       <div>
-        <h2>
-          Dashboard{" "}
+        <h5 style={thisPageCSS}>
+          Showing {count} moods entries in the database{" "}
           <span class="badge badge-secondary">history for last 2 weeks</span>
-        </h2>
-        <table className="table">
+        </h5>
+        <table style={thisPageCSS} className="table">
           <thead>
             <tr>
               <th>Mood</th>
@@ -34,8 +48,14 @@ export default class History extends Component {
                 <td>{data.scale}</td>
                 <td>{data.description}</td>
                 <td>{data.date}</td>
-                <td><button onClick={() => this.handleDelete(data)} 
-                className="btn btn-danger btn-sm">Delete</button></td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(data)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
